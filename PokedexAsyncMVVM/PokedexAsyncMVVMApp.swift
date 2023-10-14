f46e8f4bae8f20bfd23b3774feb449b426468a55
Harun Kizilkaya <h.kizilkaya@web.de>
2023-10-14 11:45:41 +0200

    Initial Commit

---
+++ PokedexAsyncMVVM/PokedexAsyncMVVMApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokedexAsyncMVVMApp.swift
+//  PokedexAsyncMVVM
+//
+//  Created by Harun Kizilkaya on 14.10.23.
+//
+
+import SwiftUI
+
+@main
+struct PokedexAsyncMVVMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
